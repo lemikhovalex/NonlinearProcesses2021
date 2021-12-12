08fa5f392fe53386499e4efcbd7a4ecf65823845
Lemikhov Alex <lemikhovalex@gmail.com>
2021-12-12 14:16:50 +0300

    runner of experiment

--- src/solver.py
+++ src/solver.py
@@ -1,13 +1,28 @@
 from enum import Enum
-from ._solver_cases import one_step_godunov
+from typing import List, Tuple
+
+from ._solver_cases import one_step_godunov, one_step_mc_cormac, one_step_holodov, one_step_forth, one_step_fifth_hybrid
 import numpy as np
 from functools import partial
 
 
 class NumericMethods(Enum):
     Godunov = partial(one_step_godunov)
+    McCormac = partial(one_step_mc_cormac)
+    Holodov = partial(one_step_holodov)
+    Forth = partial(one_step_forth)
+    Fifth = partial(one_step_fifth_hybrid)
 
 
-def one_time_step(f: np.ndarray, c: float, method: NumericMethods) -> np.ndarray:
-    return method.value(f, c)
+def process_method(method, f: np.ndarray, c: float, n_ts: int, sh: float = 0.) -> Tuple[List[np.ndarray], float]:
+    out = []
+    for _ in range(n_ts):
+        out.append(f)
+        f = method(f=f, c=c)
+        sh += c
+        if sh >= 1:
+            f = np.concatenate([f[int(sh):], np.zeros(int(sh))])
+            sh -= int(sh)
+    out.append(f)
+    return out, sh
 
